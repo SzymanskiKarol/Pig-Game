2f749c2ec959eece15561927f3f0b893f82b052b
SzymanskiKarol <szymanski.karol.1991@gmail.com>
2023-01-29 23:56:27 +0100

    final version

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ btnRoll.addEventListener('click', function () {
     if (isPlaying) {
         const dice = Math.trunc(Math.random() * 6) + 1;
         diceEl.classList.remove('hidden');
-        diceEl.src = `dice-${dice}.png`;
+        diceEl.src = `images/dice-${dice}.png`;
 
         if (dice !== 1) {
             currentScore += dice;
